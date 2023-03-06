f85a0408b345ce29b57389339cecb82e2c039a5e
Cloarec <jeremycloarec@msn.com>
2023-03-06 17:01:42 +0100

    submenu

--- bigNavbar/bigNavbar.js
+++ bigNavbar/bigNavbar.js
@@ -1,12 +1,18 @@
+//Scroll
 
 let prevScrollpos = window.pageYOffset; 
 
+
+//Menu burger 
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
+
 
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
+
+
 
 document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click", ()=>{
     hamburger.classList.remove("active");
@@ -42,11 +48,6 @@ document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click", ()
 
 
 
-
-
-
-
-
 let windowScroll = window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos ) {
@@ -57,6 +58,74 @@ let windowScroll = window.onscroll = function() {
     prevScrollpos = currentScrollPos;
 
 }
+
+//Submenu
+
+
+const navItem1 = document.querySelector("#nav-item1")
+const navItem2 = document.querySelector("#nav-item2")
+const navItem3 = document.querySelector("#nav-item3")
+const navItem4 = document.querySelector("#nav-item4")
+
+
+const subMenu = document.querySelector(".subMenu")
+const arrowDown =document.querySelector(".arrowDown")
+
+
+
+
+
+
+
+
+
+
+
+
+
+navItem1.addEventListener("mouseenter", () =>{
+    arrowDown.classList.add("arrowActive")
+})
+
+navItem1.addEventListener("mouseleave", () =>{
+    arrowDown.classList.remove("arrowActive")
+})
+
+navItem2.addEventListener("mouseenter", () =>{
+    arrowDown.classList.add("arrowActive")
+})
+
+navItem2.addEventListener("mouseleave", () =>{
+    arrowDown.classList.remove("arrowActive")
+})
+navItem3.addEventListener("mouseenter", () =>{
+    arrowDown.classList.add("arrowActive")
+})
+
+navItem3.addEventListener("mouseleave", () =>{
+    arrowDown.classList.remove("arrowActive")
+})
+navItem4.addEventListener("mouseenter", () =>{
+    arrowDown.classList.add("arrowActive")
+})
+
+navItem4.addEventListener("mouseleave", () =>{
+    arrowDown.classList.remove("arrowActive")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
